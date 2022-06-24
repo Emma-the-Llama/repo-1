e332a6a10d81203264e2a3d2e214d8c7f573cb46
Emma <emmalinbryson@gmail.com>
2022-06-24 17:06:33 +0000

    finish if elif practice and truthy falsey practice

--- Section_2:Flow_control/2.if_elif_practice.py
+++ Section_2:Flow_control/2.if_elif_practice.py
@@ -1,15 +1,27 @@
 print('Hello, what is your name?')
 Name = input()
 print('very cool! How old are you?')
-Age = input()
+Age = int(input())
 if Name == 'Emma' and Age == '14':
-    print('Oh,hi Emma!')
-elif Age < 12:
-    print('You are not Alice, kiddo')
-elif Age > 2000:
-    print('Unlike you, Alice is not a vampire')
-elif age > 100:
-    print('Okay granny, your not Alice.')
-else:
-    print('Uhh, I do not think it is possible to have a negative age but okay!')
+    print('Oh,hi Emma, creator of this program!')
+elif Name == 'Emma':
+    print('well, you have the same name as Emma, creator of this program, but you are not her age!')
+elif Age == '14':
+    print('well, you are the same age as Emma, creator of this program, but do not have her name!')
+elif Age <= 10:
+    print('You are not Emma, kiddo')
+elif Age >= 1000:
+    print('Uhhh, hello creepy extremely old being? Please get off of this program that is kinda terrifying.')
+elif Age >= 100:
+    print('Damn, impressive age! I can only begin to imagine what things you have seen in your life...')
+elif Age >= 76:
+    print('hello Granny')
+elif Age >= 58:
+    print('Ok boomer')
+elif Age >= 40:
+    print('You are many decades too old to be Emma, but hello!')
+elif Age >= 20:
+    print('How is your life going? i honestly dont know what to write for this age group lol')
+elif Age >= 11:
+    print('Oh, you are close to Emma (the creator of this programs) age!')
 print('Okay goodbye!')
--- Section_2:Flow_control/3.Truthy_Falsey_Practice.py
+++ Section_2:Flow_control/3.Truthy_Falsey_Practice.py
@@ -1,6 +1,6 @@
-print('Enter a name.')
-name = input()
-if name: #it would be better to put something like: if name != '0':, but for the sake of showing truthy and falsey it is this.
-    print('Thank you for entering something!')
+print('Please, I am very bored! Just say anything to me, anything at all. At least I will have something to think about.')
+Thing = input()
+if Thing: #it would be better to put something like: if Thing != ' ':, but for the sake of showing truthy and falsey it is this.
+    print('Thank you! This has given me life again, retrieved me from the depths of boredom. Just any simple thought is enough for me to feel alive again.')
 else:
-    print('You did not enter a name :(')
+    print('You did not enter anything... I guess I must suffer alone for eternity. :(')
