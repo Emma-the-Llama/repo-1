4aa0b7a90570e5c40a4624bfcb541e3270ce182e
Emma <emmalinbryson@gmail.com>
2022-06-25 13:55:44 +0000

    add to  rand  int  practice  and notes section 3

--- Section_3:Functions/1.rand_int_practice.py
+++ Section_3:Functions/1.rand_int_practice.py
@@ -1,2 +1,8 @@
 import random
-random.randint(1,10)
+import sys
+
+Number =  (random.randint(1,10))
+print(str(Number))
+if Number > 5:
+    sys.exit() #You could  just put if number <= 5 print another random number, but for the sake of showing sys.exit i used this.
+print(str(random.randint(1,10)) )
