56dae7e42ddfc8f19a7fe4537a25f216d7c8cf9e
Emma <emmalinbryson@gmail.com>
2022-06-25 00:11:21 +0000

    add for loop practice

---
+++ Section_2:Flow_control/6.for_loop_practice.py
@@ -0,0 +1,12 @@
+total = 0
+for num in range(101):
+    total = total + num
+print(total)
+
+#while loop equivalent
+Total = 0
+Num = 1
+while Num <= 100:
+    Total = Total + Num
+    Num = Num + 1
+print(Total)
