22212f45c9e9e3f85e176eef1575d40a3bfc64eb
Emma <emmalinbryson@gmail.com>
2022-06-26 14:01:22 +0000

    make both create function practices and add to notes section 3

---
+++ Section_3:Functions/2.create_function_practice.py
@@ -0,0 +1,14 @@
+def hello(): #creates function
+    print('Howdy!')
+    print('Howdy!!!')
+    print('hello there')
+
+hello()
+hello()
+hello()
+
+def hi(name): #name is the parameter
+    print('Hello' + name)
+
+hi(' Alice') #argument alice is assigned to parameter name
+hi(' bob') #same here
---
+++ Section_3:Functions/3.create_function_practice2.py
@@ -0,0 +1,4 @@
+def plusOne(number):
+    return number + 1
+
+plusOne(5)
