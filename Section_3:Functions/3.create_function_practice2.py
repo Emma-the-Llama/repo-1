7e9937cea9fd7ee12a15b4bc27bd7df62d95c919
Emma <emmalinbryson@gmail.com>
2022-06-26 14:34:29 +0000

    finish learning to create own functions

--- Section_3:Functions/3.create_function_practice2.py
+++ Section_3:Functions/3.create_function_practice2.py
@@ -1,4 +1,7 @@
 def plusOne(number):
     return number + 1
 
-plusOne(5)
+newNumber = plusOne(5)
+print(newNumber, end='') #this is a keyword argument
+print(' cool lol')
+print('a cat', 'dog', 'mouse', sep='&')
